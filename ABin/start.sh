ef70fc65550726595c5c0bfbd3ff56155f7d68ba
pavel <iqsf@ya.ru>
2017-04-24 14:36:19 +0300

    New script for start application.

---
+++ ABin/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#../.stack-work/install/x86_64-linux/lts-7.14/8.0.1/bin/\
+#../.stack-work/install/x86_64-linux-nopie/lts-7.14/8.0.1/bin/\
+../.stack-work/install/x86_64-linux-nopie/lts-8.11/8.0.2/bin/\
+Hydrogen-exe $@
+
+
+
